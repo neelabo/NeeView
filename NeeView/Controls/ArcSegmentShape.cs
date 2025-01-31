4d8f07b55b96e1ead08cf30bb289cd1d3334ea5a
Ito Mitsuhiro <nee.laboratory@gmail.com>
2025-01-31 18:30:02 +0900

    feat: ProcessRing 作り直し
    - 円弧座標のアニメーションで伸び縮みするようにした
    - サイズ指定できるようにした
    - ZIP削除処理中マークに採用 (#1476)

---
+++ NeeView/Controls/ArcSegmentShape.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace NeeView
+{
+    public class ArcSegmentShape : Canvas
+    {
+        private Path _path = new();
+        private PathFigure _pathFigure = new();
+        private ArcSegment _arcSegment = new();
+        private double _startAngle = 0.0;
+        private double _angle = 180.0;
+        private double _centerX;
+        private double _centerY;
+        private double _radius = 16.0;
+        private Point _startPoint;
+        private Point _endPoint;
+
+
+        public ArcSegmentShape()
+        {
+            _pathFigure.Segments.Add(_arcSegment);
+
+            PathGeometry pathGeometry = new PathGeometry();
+            pathGeometry.Figures.Add(_pathFigure);
+
+            _path.Stroke = Brushes.SteelBlue;
+            _path.StrokeThickness = 4;
+            _path.Data = pathGeometry;
+
+            Update();
+
+            this.Children.Add(_path);
+        }
+
+
+        public Brush Stroke
+        {
+            get { return (Brush)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register("Stroke", typeof(Brush), typeof(ArcSegmentShape), new PropertyMetadata(Brushes.Black, Stroke_PropertyChanged));
+
+        private static void Stroke_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ArcSegmentShape control)
+            {
+                control._path.Stroke = (Brush)e.NewValue;
+            }
+        }
+
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(ArcSegmentShape), new PropertyMetadata(1.0, StrokeThickness_PropertyChanged));
+
+        private static void StrokeThickness_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ArcSegmentShape control)
+            {
+                control._path.StrokeThickness = (double)e.NewValue;
+                control.Update();
+            }
+        }
+
+
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register("Angle", typeof(double), typeof(ArcSegmentShape), new PropertyMetadata(90.0, Angle_PropertyChanged));
+
+        private static void Angle_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ArcSegmentShape control)
+            {
+                control.UpdateAngle();
+            }
+        }
+
+
+        public double Radius
+        {
+            get { return (double)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.Register("Radius", typeof(double), typeof(ArcSegmentShape), new PropertyMetadata(16.0, Radius_PropertyChanged));
+
+        private static void Radius_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ArcSegmentShape control)
+            {
+                control.Update();
+            }
+        }
+
+
+        private void Update()
+        {
+            _radius = Radius;
+            _startPoint = GetPointOnCircle(_radius, _startAngle);
+            _pathFigure.StartPoint = _startPoint;
+            _arcSegment.Size = new Size(_radius, _radius);
+            _arcSegment.SweepDirection = SweepDirection.Clockwise;
+            UpdateAngle();
+        }
+
+        private void UpdateAngle()
+        {
+            _angle = Angle;
+            _endPoint = GetPointOnCircle(_radius, _startAngle + _angle);
+            _arcSegment.Point = _endPoint;
+            _arcSegment.IsLargeArc = _angle > 180.0; ;
+        }
+
+        private Point GetPointOnCircle(double radius, double angleInDegrees)
+        {
+            double angleInRadians = angleInDegrees * (Math.PI / 180);
+            double x = _centerX + radius * Math.Cos(angleInRadians);
+            double y = _centerY + radius * Math.Sin(angleInRadians);
+            return new Point(x, y);
+        }
+    }
+
+}
--- NeeView/Controls/ProgressRing.xaml.cs
+++ NeeView/Controls/ProgressRing.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NeeView
 {
@@ -25,10 +24,12 @@ namespace NeeView
         {
             InitializeComponent();
 
-            this.ProgressRingMark.Opacity = 0.0;
+            this.Arc.StrokeThickness = 4.0;
+            this.Arc.Opacity = 0.0;
 
             this.Loaded += (s, e) => UpdateActivity();
             this.IsVisibleChanged += (s, e) => UpdateActivity();
+            this.SizeChanged += ProgressRing_SizeChanged;
         }
 
 
@@ -41,7 +42,6 @@ namespace NeeView
         public static readonly DependencyProperty IsActiveProperty =
             DependencyProperty.Register("IsActive", typeof(bool), typeof(ProgressRing), new PropertyMetadata(true, IsActiveProperty_Changed));
 
-
         private static void IsActiveProperty_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is ProgressRing control)
@@ -51,26 +51,69 @@ namespace NeeView
         }
 
 
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(ProgressRing), new PropertyMetadata(0.0, ValueProperty_Changed));
+
+        private static void ValueProperty_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ProgressRing control)
+            {
+                control.UpdateValue();
+            }
+        }
+
+
+        private void ProgressRing_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var radius = Math.Max((Math.Min(e.NewSize.Width, e.NewSize.Height) - this.Arc.StrokeThickness) * 0.5, 1.0);
+            this.Arc.Radius = radius;
+        }
+
         private void UpdateActivity()
         {
             if (IsActive && IsVisible)
             {
                 var ani = new DoubleAnimation(1, TimeSpan.FromSeconds(0.0)) { BeginTime = TimeSpan.FromSeconds(0.0) };
-                this.ProgressRingMark.BeginAnimation(UIElement.OpacityProperty, ani, HandoffBehavior.SnapshotAndReplace);
+                this.Arc.BeginAnimation(UIElement.OpacityProperty, ani, HandoffBehavior.SnapshotAndReplace);
 
                 var aniRotate = new DoubleAnimation();
                 aniRotate.By = 360;
-                aniRotate.Duration = TimeSpan.FromSeconds(2.0);
+                aniRotate.Duration = TimeSpan.FromSeconds(0.8);
                 aniRotate.RepeatBehavior = RepeatBehavior.Forever;
-                this.ProgressRingMarkAngle.BeginAnimation(RotateTransform.AngleProperty, aniRotate);
+                this.ArcRotate.BeginAnimation(RotateTransform.AngleProperty, aniRotate);
+
+                var aniValue = new DoubleAnimation();
+                aniValue.From = 0.0;
+                aniValue.To = 1.0;
+                aniValue.Duration = TimeSpan.FromSeconds(2.0);
+                aniValue.RepeatBehavior = RepeatBehavior.Forever;
+                this.BeginAnimation(ProgressRing.ValueProperty, aniValue);
             }
             else
             {
-                this.ProgressRingMark.BeginAnimation(UIElement.OpacityProperty, null, HandoffBehavior.SnapshotAndReplace);
-                this.ProgressRingMark.Opacity = 0.0;
+                this.Arc.BeginAnimation(UIElement.OpacityProperty, null, HandoffBehavior.SnapshotAndReplace);
+                this.Arc.Opacity = 0.0;
 
-                this.ProgressRingMarkAngle.BeginAnimation(RotateTransform.AngleProperty, null, HandoffBehavior.SnapshotAndReplace);
+                this.ArcRotate.BeginAnimation(RotateTransform.AngleProperty, null, HandoffBehavior.SnapshotAndReplace);
+
+                this.BeginAnimation(ProgressRing.ValueProperty, null, HandoffBehavior.SnapshotAndReplace);
             }
         }
+
+        private void UpdateValue()
+        {
+            // Arc.Angle vary between 90 and 270.
+            double radian = Value * Math.PI * 2.0;
+            var deltaAngle = 90.0 * Math.Sin(radian);
+            this.Arc.Angle = 180.0 + deltaAngle;
+        }
+
     }
+
 }
