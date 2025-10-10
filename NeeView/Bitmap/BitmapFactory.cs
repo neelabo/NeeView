e7841d3dd5d4b366647ffe21c5defdee2821266e
Ito Mitsuhiro <nee.laboratory@gmail.com>
2025-10-10 16:02:16 +0900

    fix: サードパーティー製WICの場合でリサイズフィルターを機能するようにした (#1809)
    MagicScalerでサポート外のフォーマットの場合、標準機能でいったんTIFF画像に変換してから処理するようにした

--- NeeView/Bitmap/BitmapFactory.cs
+++ NeeView/Bitmap/BitmapFactory.cs
@@ -25,7 +25,22 @@ namespace NeeView
             {
                 try
                 {
-                    return _magicScaler.Create(stream, info, size, setting.ProcessImageSettings);
+                    var pos = stream.Position;
+                    try
+                    {
+                        return _magicScaler.Create(stream, info, size, setting.ProcessImageSettings);
+                    }
+                    catch (InvalidDataException ex)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        Debug.WriteLine("MagicScaler Failed:" + ex.Message);
+                        stream.Position = pos;
+                        return CreateBitmapSourceWithMagicScaler(stream, info, size, setting, token);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    throw;
                 }
                 catch (OutOfMemoryException)
                 {
@@ -40,6 +55,28 @@ namespace NeeView
 
             // by Default
             return _default.Create(stream, info, size, token);
+        }
+
+        /// <summary>
+        /// Decode the image using standard functions, then process it with MagicScaler.
+        /// </summary>
+        /// <remarks>
+        /// This is the processing for WIC that MagicScaler does not support.
+        /// </remarks>
+        private BitmapImage CreateBitmapSourceWithMagicScaler(Stream stream, BitmapInfo? info, Size size, BitmapCreateSetting setting, CancellationToken token)
+        {
+            // convert stream to raw size BitmapImage using default decoder
+            BitmapSource bitmap = _default.Create(stream, info, Size.Empty, token);
+
+            // convert bitmap to TIFF stream
+            var encoder = new TiffBitmapEncoder() { Compression = TiffCompressOption.None };
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            using var ms = new MemoryStream();
+            encoder.Save(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            // convert TIFF stream to BitmapImage using MagicScaler
+            return _magicScaler.Create(ms, info, size, setting.ProcessImageSettings);
         }
 
         public void CreateImage(Stream stream, BitmapInfo? info, Stream outStream, Size size, BitmapImageFormat format, int quality, BitmapCreateSetting setting, CancellationToken token)
