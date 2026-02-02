6c755ff4aa867175898cd635b455542bb4afc1b5
nee (Ito Mitsuhiro) <nee.laboratory@gmail.com>
2026-02-02 16:59:21 +0900

    chore: Moved old changelogs to a separate page

--- docs/assets/js/issue-linker.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const body = document.body;
-    body.innerHTML = body.innerHTML.replace(/#(\d+)/g, `<a href="${repoUrl}/issues/$1">#$1</a>`);
-});
