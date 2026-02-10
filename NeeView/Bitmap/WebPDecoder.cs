f867dd8db60a38ea5d33b3e89520f239f1b68f34
nee (Ito Mitsuhiro) <nee.laboratory@gmail.com>
2026-02-10 19:58:23 +0900

    fix: アニメーションWebPを静止画として表示できない不具合修正 (#1865)
    - アニメーションWebPの場合は最初の1フレームを取得するようにした

--- NeeView/Bitmap/WebPDecoder.cs
+++ NeeView/Bitmap/WebPDecoder.cs
@@ -8,14 +8,85 @@ namespace NeeView
     {
         private static class NativeMethods
         {
-            [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libwebp", CallingConvention = CallingConvention.Cdecl)]
             public static extern int WebPGetInfo(IntPtr data, UIntPtr data_size, out int width, out int height);
 
-            [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libwebp", CallingConvention = CallingConvention.Cdecl)]
             public static extern IntPtr WebPDecodeBGRAInto(IntPtr data, UIntPtr data_size, IntPtr output_buffer, UIntPtr output_buffer_size, int output_stride);
 
-            [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libwebp", CallingConvention = CallingConvention.Cdecl)]
             public static extern void WebPFree(IntPtr ptr);
+
+            [DllImport("libwebp", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int WebPGetFeaturesInternal(IntPtr data, UIntPtr data_size, ref WebPBitstreamFeatures features, int abi_version);
+
+            public const int WEBP_DECODER_ABI_VERSION = 0x0209;
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct WebPBitstreamFeatures
+            {
+                public int width;
+                public int height;
+                public int has_alpha;
+                public int has_animation;
+                public int format;
+                private readonly uint pad1;
+                private readonly uint pad2;
+                private readonly uint pad3;
+                private readonly uint pad4;
+                private readonly uint pad5;
+            }
+
+            [DllImport("libwebpdemux", CallingConvention = CallingConvention.Cdecl)]
+            public static extern IntPtr WebPAnimDecoderNewInternal(ref WebPData webp_data, ref WebPAnimDecoderOptions dec_options, int abi_version);
+
+            public const int WEBP_DEMUX_ABI_VERSION = 0x0107;
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct WebPData
+            {
+                public IntPtr data;
+                public UIntPtr size;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct WebPAnimDecoderOptions
+            {
+                public int color_mode;
+                public int use_threads;
+                private readonly uint pad1;
+                private readonly uint pad2;
+                private readonly uint pad3;
+                private readonly uint pad4;
+                private readonly uint pad5;
+                private readonly uint pad6;
+                private readonly uint pad7;
+            };
+
+            public const int WEBP_CSP_MODE_BGRA = 3;
+
+            [DllImport("libwebpdemux", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int WebPAnimDecoderGetInfo(IntPtr dec, out WebPAnimInfo info);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct WebPAnimInfo
+            {
+                public uint canvas_width;
+                public uint canvas_height;
+                public uint loop_count;
+                public uint bgcolor;
+                public uint frame_count;
+                private readonly uint pad1;
+                private readonly uint pad2;
+                private readonly uint pad3;
+                private readonly uint pad4;
+            }
+
+            [DllImport("libwebpdemux", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int WebPAnimDecoderGetNext(IntPtr dec, out IntPtr buf, out int timestamp);
+
+            [DllImport("libwebpdemux", CallingConvention = CallingConvention.Cdecl)]
+            public static extern void WebPAnimDecoderDelete(IntPtr dec);
         }
 
 
@@ -29,49 +100,102 @@ namespace NeeView
             }
         }
 
-        public static bool IsWebP(byte[] webpData)
+        public static bool IsWebP(byte[] bytes)
         {
-            return AnimatedImageChecker.IsWebp(new MemoryStream(webpData, false));
+            return AnimatedImageChecker.IsWebp(new MemoryStream(bytes, false));
         }
 
-        public static RawImage Decode(byte[] webpData)
+        public static RawImage Decode(byte[] bytes)
         {
             Initialize();
 
-            var webpDataHandle = GCHandle.Alloc(webpData, GCHandleType.Pinned);
-            var webpDataPtr = webpDataHandle.AddrOfPinnedObject();
+            var webpDataHandle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            var webpData = new NativeMethods.WebPData();
+            webpData.data = webpDataHandle.AddrOfPinnedObject();
+            webpData.size = (UIntPtr)bytes.Length;
+
             try
             {
-                var result = NativeMethods.WebPGetInfo(webpDataPtr, (UIntPtr)webpData.Length, out int width, out int height);
-                if (result == 0)
+                var features = new NativeMethods.WebPBitstreamFeatures();
+                if (NativeMethods.WebPGetFeaturesInternal(webpData.data, webpData.size, ref features, NativeMethods.WEBP_DECODER_ABI_VERSION) != 0)
                 {
-                    throw new WebPDecoderException("WebPGetInfo failed");
+                    throw new WebPDecoderException("WebPGetFeatures failed");
                 }
 
-                int stride = width * 4;
-                int bufferSize = stride * height;
-                byte[] buffer = new byte[bufferSize];
-
-                var bufferHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
-                var bufferPtr = bufferHandle.AddrOfPinnedObject();
-                try
+                if (features.has_animation == 0)
                 {
-                    var ptr = NativeMethods.WebPDecodeBGRAInto(webpDataPtr, (UIntPtr)webpData.Length, bufferPtr, (UIntPtr)buffer.Length, stride);
-                    if (ptr == IntPtr.Zero)
-                    {
-                        throw new WebPDecoderException("WebPDecode failed");
-                    }
-
-                    return new RawImage(buffer, width, height);
+                    return DecodeImage(webpData, features.width, features.height);
                 }
-                finally
+                else
                 {
-                    bufferHandle.Free();
+                    return DecodeFirstFrame(webpData);
                 }
             }
             finally
             {
                 webpDataHandle.Free();
+            }
+        }
+
+        private static RawImage DecodeImage(NativeMethods.WebPData webpData, int width, int height)
+        {
+            int stride = width * 4;
+            int bufferSize = stride * height;
+            byte[] buffer = new byte[bufferSize];
+            var bufferHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            var bufferPtr = bufferHandle.AddrOfPinnedObject();
+
+            try
+            {
+                var ptr = NativeMethods.WebPDecodeBGRAInto(webpData.data, webpData.size, bufferPtr, (UIntPtr)buffer.Length, stride);
+                if (ptr == IntPtr.Zero)
+                {
+                    throw new WebPDecoderException("WebPDecode failed");
+                }
+                return new RawImage(buffer, width, height);
+            }
+            finally
+            {
+                bufferHandle.Free();
+            }
+        }
+
+        private static RawImage DecodeFirstFrame(NativeMethods.WebPData webpData)
+        {
+            var options = new NativeMethods.WebPAnimDecoderOptions();
+            options.color_mode = NativeMethods.WEBP_CSP_MODE_BGRA;
+
+            IntPtr decoder = IntPtr.Zero;
+            try
+            {
+                decoder = NativeMethods.WebPAnimDecoderNewInternal(ref webpData, ref options, NativeMethods.WEBP_DEMUX_ABI_VERSION);
+                if (decoder == 0)
+                {
+                    throw new WebPDecoderException("WebPAnimDecoderNew failed");
+                }
+
+                if (NativeMethods.WebPAnimDecoderGetInfo(decoder, out NativeMethods.WebPAnimInfo info) == 0)
+                {
+                    throw new WebPDecoderException("WebPAnimDecoderGetInfo failed");
+                }
+
+                if (NativeMethods.WebPAnimDecoderGetNext(decoder, out IntPtr rgbaPtr, out int timestamp) == 0)
+                {
+                    throw new WebPDecoderException("WebPAnimDecoderGetNext failed");
+                }
+
+                int stride = (int)info.canvas_width * 4;
+                int bufferSize = stride * (int)info.canvas_height;
+                byte[] buffer = new byte[bufferSize];
+                Marshal.Copy(rgbaPtr, buffer, 0, buffer.Length);
+                return new RawImage(buffer, (int)info.canvas_width, (int)info.canvas_height);
+            }
+            finally
+            {
+                if (decoder != IntPtr.Zero)
+                {
+                    NativeMethods.WebPAnimDecoderDelete(decoder);
+                }
             }
         }
     }
